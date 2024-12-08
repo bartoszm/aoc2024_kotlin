793773160025ea9501274cb664a82bf0221374a2
bartoszm <bartosz.michalik@gmail.com>
2024-12-08 08:04:44 +0100

    day 8 - refactor for reuse

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -21,11 +21,11 @@ fun main() {
         return input.flatMapIndexed { i, l -> parse(i, l).toList() }
     }
 
-    fun List<Point>.combine(): List<Pair<Point,Point>> {
+    fun List<Point>.combine(): List<Pair<Point, Point>> {
         return sequence {
             val l = this@combine
-            for(i in l.indices) {
-                for(j in i + 1 until l.size) {
+            for (i in l.indices) {
+                for (j in i + 1 until l.size) {
                     yield(l[i] to l[j])
                 }
             }
@@ -35,35 +35,28 @@ fun main() {
     fun List<String>.inB(at: Point) = at.first in indices && at.second in this[0].indices
     fun distance(a: Point, b: Point) = (b.first - a.first) to (b.second - a.second)
 
-    fun part1(input: List<String>): Int {
+    data class Context(val a: Point, val b: Point, val distance: Point)
+
+    fun process(input: List<String>, generator: (Context) -> Sequence<Point>): Int {
         val data = parse(input).groupBy { it.v }
 
         val antiAntennas = data.values.flatMap { v ->
             v.map { it.p }.combine()
-                .flatMap { (a, b) ->
-                    val d = distance(a, b)
-                    listOf(a - d, b + d)
-                }
-        }.distinct()
-            .filter { input.inB(it) }
-        return antiAntennas.count()
-    }
-
-    fun part2(input: List<String>): Int {
-        val data = parse(input).groupBy { it.v }
-
-        val antiAntennas = data.values.flatMap { v ->
-            v.map { it.p }.combine()
-                .flatMap { (a, b) ->
-                    val d = distance(a, b)
-                    generateSequence(a) { acc -> acc - d }.takeWhile { input.inB(it) } +
-                            generateSequence(b) { acc -> acc + d }.takeWhile { input.inB(it) }
-                }
+                .flatMap { (a, b) -> generator(Context(a, b, distance(a, b))) }
         }.distinct()
         return antiAntennas.count()
     }
 
-    val tiny = readInput("day08","tiny")
+    fun part1(input: List<String>) = process(input) { (a, b, d) ->
+        sequenceOf(a - d, b + d).filter { k -> input.inB(k) }
+    }
+
+    fun part2(input: List<String>) = process(input) { (a, b, d) ->
+        generateSequence(a) { acc -> acc - d }.takeWhile { input.inB(it) } +
+                generateSequence(b) { acc -> acc + d }.takeWhile { input.inB(it) }
+    }
+
+    val tiny = readInput("day08", "tiny")
     val trial = readInput("day08")
     val input = readInput("day08", "input")
     part1(tiny).println()
